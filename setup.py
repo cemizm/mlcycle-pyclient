aa9cf5cb4c174b8daef936122cfd12f47768faf1
Cem Basoglu <cem.basoglu@web.de>
2019-07-24 15:42:15 +0200

    using build environment variables in setup.py is a bad idea…
    deploy manually and use hardcoded version number

--- setup.py
+++ setup.py
@@ -4,16 +4,9 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-if os.environ.get('CI_COMMIT_TAG'):
-    version = os.environ['CI_COMMIT_TAG']
-elif os.environ.get('CI_JOB_ID'):
-    version = os.environ['CI_JOB_ID']
-else:
-    version = "0.0.1"
-
 setuptools.setup(
     name="mlcycle",
-    version=version,
+    version=0.1.5,
     url="http://git01-ifm-min.ad.fh-bielefeld.de/pvserve2/mlcycle",
     author="Cem Basoglu",
     author_email="cem.basoglu@fh-bielefeld.de",
