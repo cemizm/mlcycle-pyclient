018223aedc435a1f6fb5531bdf1ccc4d490be124
Cem Basoglu <cem.basoglu@web.de>
2019-07-24 10:43:56 +0200

    rename library

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="contiflow",
+    name="mlcycle",
     version="0.0.1",
-    url="http://git01-ifm-min.ad.fh-bielefeld.de/pvserve2/contiflow",
+    url="http://git01-ifm-min.ad.fh-bielefeld.de/pvserve2/mlcycle",
     author="Cem Basoglu",
     author_email="cem.basoglu@fh-bielefeld.de",
-    description="Client library for communication with contiflow backend.",
+    description="MLCycle client library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT',
